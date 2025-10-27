f561d1ad0d618632cf0fe3a6f1d9a1ad1712841d
Ibrahim Kashif <56864637+ibbibrahim@users.noreply.github.com>
2025-10-27 11:23:07 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mkcert from "vite-plugin-mkcert"; // plugin for local HTTPS
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "localhost",
     port: 5173,
-    https: {}, // ✅ Empty object → mkcert will inject HTTPS certs automatically
   },
   plugins: [
     react(),
-    mkcert(), // ✅ Automatically generates and trusts certificates
   ],
   optimizeDeps: {
     exclude: ["lucide-react"],
